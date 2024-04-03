99179bdc2accd2f39c1d745297a763ba524217ac
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-02 23:33:35 -0400

    New commit

--- src/Utility/userHelperDB.py
+++ src/Utility/userHelperDB.py
@@ -1,14 +1,16 @@
 
 from src.DAO.connectDAO import ConnectDAO
 import random
-
+import allure
 class UserHelperDB(object):
     def __init__(self):
-        self.execute = ConnectDAO()
+        with allure.step("Connect to DB"):
+            self.execute = ConnectDAO()
+
 
     def get_random_email_from_DB(self, qty=1):
+        with allure.step("Execute SELECT"):
+            sql = f"SELECT * FROM users WHERE `group`='APG777' order by id ASC LIMIT 10;"
+            exec_db = self.execute.execute_SELECT(sql)
 
-        sql = f"SELECT * FROM users WHERE `group`='APG777' order by id ASC LIMIT 10;"
-        exec_db = self.execute.execute_SELECT(sql)
-
-        return random.sample(exec_db, qty)
+            return random.sample(exec_db, qty)
--- tests/smoke_test/smoke_test.py
+++ tests/smoke_test/smoke_test.py
@@ -5,16 +5,14 @@ import allure
 import pytest
 import logging as log
 
-@allure.feature('Healthy check')
-@pytest.mark.tcid01
-def test_healthcheck():
-    log.info("Hello World")
-
-
+@allure.feature("Checking DB connection")
 @pytest.mark.tcid02
 def test_random_email_from_DB():
     exec = UserHelperDB()
     result = exec.get_random_email_from_DB()
+    group_ID = "APG777"
+    # import pdb; pdb.set_trace()
+    assert result[0]['group'] == group_ID, f"The expected group: {group_ID} but returned group: {result[0]['group']}"
 
 
 
