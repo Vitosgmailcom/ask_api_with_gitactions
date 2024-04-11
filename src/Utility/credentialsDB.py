dd0c46b5d4ecb4b28ed09354dc294d0ac6c4b254
vitalykamynin <vitaly.kamynin@gmail.com>
2024-04-10 23:43:30 -0400

    test7

--- src/Utility/credentialsDB.py
+++ src/Utility/credentialsDB.py
@@ -2,11 +2,11 @@ import logging as log
 import os
 
 def credentialsDB():
-    db_user = os.getenv('db_user')
-    db_pass = os.getenv('db_pass')
-    db_name = os.getenv('db_name')
-    db_port = os.getenv('db_port')
-    db_host = os.getenv('db_host')
+    db_user = os.getenv('DB_USER')
+    db_pass = os.getenv('DB_PASS')
+    db_name = os.getenv('DB_NAME')
+    db_port = os.getenv('DB_PORT')
+    db_host = os.getenv('DB_HOST')
 
     if not db_user and not db_pass:
         raise Exception(f"The db_user and db_pass must be set up")
